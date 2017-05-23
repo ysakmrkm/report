bb77066019127cb4c555cd00e4254f8b40f9ffae
ysakmrkm <info@ysakmrkm.com>
2017-05-23 14:15:32 +0900

    Change to function and execute at regular interval

--- main.js
+++ main.js
@@ -21,134 +21,143 @@ let tasks = [];
 let yesterday = new Date().setDate(new Date().getDate() - 1);
 yesterday = new Date(yesterday);
 
-// # Trello ボードのカードチェック
-Promise.resolve()
-.then(()=> {
-  // 1. ボード情報取得
-  return new Promise((resolve, reject)=> {
-    trello.get('/1/members/me/boards', function(err, data) {
-      if (err) {
-        reject(err);
-        return;
-      }
+// チェック間隔 5分
+const intervalSec = 5 * 60 * 1000;
 
-      let board = data.filter((val) => {
-        return val.id === configs.trello.boardId;
-      });
-
-      resolve(board[0]);
-    });
-  });
-}).then((data)=> {
-  // 2. ボードからリスト取得
-  return new Promise((resolve, reject)=> {
-    trello.get('/1/boards/'+data.id+'/lists', function(err, data) {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      let list = data.filter((val) => {
-        return val.name === configs.trello.listName;
-      });
-
-      resolve(list[0]);
-    });
-  });
-}).then((data)=> {
-  // 3. リストからカード取得
-  return new Promise((resolve, reject)=> {
-    trello.get('/1/lists/'+data.id+'/cards', function(err, data) {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      data.forEach((current, index, array)=> {
-        cards.push({
-          'title': current.name,
-          'date': new Date(1000*parseInt(current.id.substring(0,8),16))
-        });
-      })
-
-      resolve(cards);
-    });
-  });
-}).then((data)=> {
-  // # 追加されたカードを Redmine のタスク管理用プロジェクトでチケット発行
-  // 1. Redmine のチケットチェック
-
-  return new Promise((resolve, reject)=> {
-    redmine.issues({limit: 100, project_id: configs.redmine.projectId}, (err, data)=> {
-      if (err) throw err;
-
-      topTasks = data.issues.filter((issue)=> {
-        return issue.parent === undefined
-      });
-
-      topTasks.forEach((current, index, array)=> {
-        tasks.push({
-          'title': current.subject,
-          'date': current.created_on
-        });
-      })
-
-      resolve(tasks);
-    });
-  });
-}).then((data)=> {
-  // 2. Trello のカードと比較
-
-  newIssues = [];
-
-  return new Promise((resolve, reject)=> {
-    cards.forEach((currentCard, cardIndex, cardArray)=> {
-      taskExist = false;
-
-      tasks.forEach((currentTask, taskIndex, taskArray)=> {
-        if(currentTask.title.indexOf(currentCard.title) !== -1) {
-          taskExist = true;
-        }
-      });
-
-      let date = new Date(currentCard.date).getTime();
-
-      // ## 作成日が24時間以内のタスクをチェック
-      if(!taskExist && date >= yesterday.getTime()) {
-        newIssues.push(currentCard);
-      }
-    });
-
-    resolve(newIssues);
-  });
-}).then((data)=> {
-  // 3. 無ければチケット発行
-
-  let newIssues = data;
-
-  // ## ユーザーID取得
-  redmine.current_user({include: "memberships,groups"}, function(err, data) {
-    if (err) throw err;
-
-    newIssues.forEach((currentIssue, issueIndex, issueArray)=> {
-      let issue = {
-        'issue': {
-          project_id: configs.redmine.projectId,
-          assigned_to_id: data.user.id,
-          subject: currentIssue.title,
-        }
-      };
-
-      // ## チケット発行
-      redmine.create_issue(issue, (err, data)=> {
+// # Trello ボードのカードチェック / Redmine プロジェクトの issue チェック・発行
+createNewIssues = ()=> {
+  Promise.resolve()
+  .then(()=> {
+    // 1. ボード情報取得
+    return new Promise((resolve, reject)=> {
+      trello.get('/1/members/me/boards', function(err, data) {
         if (err) {
-          console.log("Error: " + err.message);
+          reject(err);
           return;
         }
+
+        let board = data.filter((val) => {
+          return val.id === configs.trello.boardId;
+        });
+
+        resolve(board[0]);
+      });
+    });
+  }).then((data)=> {
+    // 2. ボードからリスト取得
+    return new Promise((resolve, reject)=> {
+      trello.get('/1/boards/'+data.id+'/lists', function(err, data) {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        let list = data.filter((val) => {
+          return val.name === configs.trello.listName;
+        });
+
+        resolve(list[0]);
+      });
+    });
+  }).then((data)=> {
+    // 3. リストからカード取得
+    return new Promise((resolve, reject)=> {
+      trello.get('/1/lists/'+data.id+'/cards', function(err, data) {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        data.forEach((current, index, array)=> {
+          cards.push({
+            'title': current.name,
+            'date': new Date(1000*parseInt(current.id.substring(0,8),16))
+          });
+        })
+
+        resolve(cards);
+      });
+    });
+  }).then((data)=> {
+    // # 追加されたカードを Redmine のタスク管理用プロジェクトでチケット発行
+    // 1. Redmine のチケットチェック
+
+    return new Promise((resolve, reject)=> {
+      redmine.issues({limit: 100, project_id: configs.redmine.projectId}, (err, data)=> {
+        if (err) throw err;
+
+        topTasks = data.issues.filter((issue)=> {
+          return issue.parent === undefined
+        });
+
+        topTasks.forEach((current, index, array)=> {
+          tasks.push({
+            'title': current.subject,
+            'date': current.created_on
+          });
+        })
+
+        resolve(tasks);
+      });
+    });
+  }).then((data)=> {
+    // 2. Trello のカードと比較
+
+    newIssues = [];
+
+    return new Promise((resolve, reject)=> {
+      cards.forEach((currentCard, cardIndex, cardArray)=> {
+        taskExist = false;
+
+        tasks.forEach((currentTask, taskIndex, taskArray)=> {
+          if(currentTask.title.indexOf(currentCard.title) !== -1) {
+            taskExist = true;
+          }
+        });
+
+        let date = new Date(currentCard.date).getTime();
+
+        // ## 作成日が24時間以内のタスクをチェック
+        if(!taskExist && date >= yesterday.getTime()) {
+          newIssues.push(currentCard);
+        }
+      });
+
+      resolve(newIssues);
+    });
+  }).then((data)=> {
+    // 3. 無ければチケット発行
+
+    let newIssues = data;
+
+    // ## ユーザーID取得
+    redmine.current_user({include: "memberships,groups"}, function(err, data) {
+      if (err) throw err;
+
+      newIssues.forEach((currentIssue, issueIndex, issueArray)=> {
+        let issue = {
+          'issue': {
+            project_id: configs.redmine.projectId,
+            assigned_to_id: data.user.id,
+            subject: currentIssue.title,
+          }
+        };
+
+        // ## チケット発行
+        redmine.create_issue(issue, (err, data)=> {
+          if (err) {
+            console.log("Error: " + err.message);
+            return;
+          }
+        });
       });
     });
   });
-});
+
+  setTimeout(createNewIssues, intervalSec);
+}
+
+createNewIssues();
 
 // TODO
 // # Toggl で作業時間計測
